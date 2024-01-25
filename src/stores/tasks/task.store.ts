9d1a4ef3728548fce3c69a6c5f5c3e6153b2d3e8
bryan lopez <blopez4434@gmail.com>
2024-01-25 02:33:37 -0300

    Add dragging functionality to SingleTask component

--- src/stores/tasks/task.store.ts
+++ src/stores/tasks/task.store.ts
@@ -1,13 +1,19 @@
 import { StateCreator, create } from "zustand";
 import type { Task, TaskStatus } from "../../interfaces";
+import { devtools } from "zustand/middleware";
 
 interface TaskState {
+  draggingTaskId?: string;
   tasks: Record<string, Task>; // { [key: string]: Task }
 
   getTaskByStatus: (status: TaskStatus) => Task[];
+  setDraggingTaskId: (taskId: string) => void;
+  removeDraggingTaskId: () => void;
 }
 
 const storeApi: StateCreator<TaskState> = (set, get) => ({
+  //   no es necesario ponerlo porque automatico lo define como undefined
+  grabbingTaskId: undefined,
   tasks: {
     "ABC-1": { id: "ABC-1", title: "Task 1", status: "open" },
     "ABC-2": { id: "ABC-2", title: "Task 2", status: "open" },
@@ -17,6 +23,8 @@ const storeApi: StateCreator<TaskState> = (set, get) => ({
   getTaskByStatus: (status: TaskStatus) => {
     return Object.values(get().tasks).filter((task) => task.status === status);
   },
+  setDraggingTaskId: (taskId: string) => set({ draggingTaskId: taskId }),
+  removeDraggingTaskId: () => set({ draggingTaskId: undefined }),
 });
 
-export const useTaskStore = create<TaskState>()(storeApi);
+export const useTaskStore = create<TaskState>()(devtools(storeApi));
